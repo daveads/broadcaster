5e4a5e8d24e95c5404eee9ca7b754051b02d7802
daveads <the.daveads@gmail.com>
2024-09-25 21:21:11 +0100

    ok

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -1,4 +1,5 @@
 import pytest
+import asyncio
 
 from broadcaster import Broadcast
 
@@ -31,6 +32,7 @@ async def test_redis():
 async def test_pulsar():
     async with Broadcast("pulsar://localhost:6650") as broadcast:
         async with broadcast.subscribe("chatroom") as subscriber:
+            await asyncio.sleep(0.5)
             await broadcast.publish("chatroom", "hello")
             event = await subscriber.get()
             assert event.channel == "chatroom"
